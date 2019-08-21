925d0c9d5b3a1c389d28530eb4ceffeeb84159ec
jchheng <james@Jamess-MacBook-Pro.local>
2019-08-21 13:56:03 -0700

    print out request body when api exception is thrown to help with debugging

--- lib/capital-iq/api_error.rb
+++ lib/capital-iq/api_error.rb
@@ -1,9 +1,10 @@
 module CapitalIQ
   class ApiError < StandardError
-    attr_reader :response_data
-    def initialize(response_data)
+    attr_reader :response_data, :request_body
+    def initialize(request_body, response_data)
+      @request_body = request_body
       @response_data = response_data
-      super("Capital IQ API returned an error. Response content: #{self.inspect}")
+      super("Capital IQ API returned an error. Request content: #{request_body.to_json} Response content: #{response_data.to_json}")
     end
 
     def inspect
